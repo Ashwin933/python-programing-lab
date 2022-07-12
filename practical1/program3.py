a32efec4144f56adb56f87ca8afed96446444cb2
Ashwin933 <108949656+Ashwin933@users.noreply.github.com>
2022-07-12 18:55:01 +0530

    Add files via upload

---
+++ practical1/program3.py
@@ -0,0 +1,6 @@
+#python program to find the square root
+import math
+print("This is a Python program to find the square root of a number")
+n=int(input("Enter a number: "))
+sq=math.sqrt(n)
+print("The square root of",n,"is",sq)
---
+++ practical1/program4.py
@@ -0,0 +1,6 @@
+#Python program to calculate the area of a triangle
+print("This is a python program to calculate the area of a triangle. Kindly enter the dimensions of height and base")
+H=int(input("Enter height: "))
+B=int(input("Enter base: "))
+area=0.5*H*B
+print("The area of triangle is",area,"sq.units")
